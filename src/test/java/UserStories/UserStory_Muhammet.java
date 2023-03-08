18aa4871f419aed91428eafe1c0c897ca6352f4b
m.tastan <m.tastan@outlook.com>
2023-03-08 15:44:54 -0600

    The Test has been successfully completed.

--- src/test/java/UserStories/UserStory_Muhammet.java
+++ src/test/java/UserStories/UserStory_Muhammet.java
@@ -2,7 +2,53 @@ package UserStories;
 
 import Utilities.BaseDriver;
 import Utilities.Tools.Tools_Muhammet;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class UserStory_Muhammet extends Tools_Muhammet {
+
+    // Test Steps
+
+    //1-In the login Panel, enter the username
+    //2-Enter the Password for the ESS-User account in the password field
+    //3-Click "Login" button
+
+    //Expected Results:
+    //The user is logged in successfully.
+
+    @Test
+    void successfulLogged(){
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(1000));
+
+
+       driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+       wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='Tom Collings']")));
+        WebElement userDropdownName = driver.findElement(By.xpath("//p[text()='Tom Collings']"));
+
+        Assert.assertTrue(userDropdownName.isDisplayed());
+        Assert.assertEquals(userDropdownName.getText(), "Tom Collings");
+
+
+    }
 
 }
--- src/test/java/Utilities/Tools/Tools_Muhammet.java
+++ src/test/java/Utilities/Tools/Tools_Muhammet.java
@@ -1,6 +1,19 @@
 package Utilities.Tools;
 
+
 import Utilities.BaseDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 public class Tools_Muhammet extends BaseDriver {
+
 }
+
