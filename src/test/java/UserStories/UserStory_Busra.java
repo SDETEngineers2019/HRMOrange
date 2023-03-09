d88bb5cdedf4e6649f4108986fe6bbe6f5f3bafa
busrav <busravural67@gmail.com>
2023-03-09 11:36:44 -0500

    Edit name test All documents are ready.

--- src/test/java/UserStories/UserStory_Busra.java
+++ src/test/java/UserStories/UserStory_Busra.java
@@ -1,6 +1,53 @@
 package UserStories;
 
 import Utilities.Tools.Tools_Busra;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-public class UserStory_Busra extends Tools_Busra {
+import java.time.Duration;
+
+public class UserStory_Busra extends Tools_Busra{
+
+    @Test
+    @Parameters(value = "keyWord")
+    void editName(String name){
+        driver.get(" https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        visibilityOfElement(By.xpath("//input[@name='username']"));
+        WebElement username = driver.findElement(By.xpath("//input[@name='username']"));
+        username.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys("admin123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+
+        visibilityOfElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a"));
+        WebElement myInfo = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a"));
+        myInfo.click();
+
+        visibilityOfElement(By.cssSelector("input[name='firstName']"));
+        WebElement firstName = driver.findElement(By.cssSelector("input[name='firstName']"));
+        firstName.click();
+
+        String actualName = firstName.getText();
+        String selectAll = Keys.chord(Keys.CONTROL, "a");
+        firstName.sendKeys(selectAll);
+        firstName.sendKeys(Keys.DELETE);
+        firstName.sendKeys(name);
+
+        WebElement saveButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        saveButton.click();
+
+        driver.navigate().refresh();
+
+        Assert.assertNotEquals(actualName,"Mark", "test failed");
+
+    }
 }
--- src/test/java/Utilities/Tools/Tools_Busra.java
+++ src/test/java/Utilities/Tools/Tools_Busra.java
@@ -1,7 +1,20 @@
 package Utilities.Tools;
 
 import Utilities.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
 
 public class Tools_Busra extends BaseDriver {
+
+public static WebDriverWait wait;
+
+    public void visibilityOfElement(By locator){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 }
