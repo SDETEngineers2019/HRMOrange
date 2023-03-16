8a20f2ed2968e9f1723245394f952f6fb3a94288
busrav <busravural67@gmail.com>
2023-03-16 12:42:52 -0400

    Edit name test All documents are ready.

--- src/test/java/Utilities/Tools/Tools_Busra.java
+++ src/test/java/Utilities/Tools/Tools_Busra.java
@@ -2,8 +2,11 @@ package Utilities.Tools;
 
 import Utilities.BaseDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 
 import java.time.Duration;
@@ -13,8 +16,60 @@ public class Tools_Busra extends BaseDriver {
 public static WebDriverWait wait;
 
     public void visibilityOfElement(By locator){
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait = new WebDriverWait(BaseDriver.driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void waitUntilVisible(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitUntilClickable(WebElement element){
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void scrollToElement(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+    public void sendKeysMethod(WebElement element,String keys){
+        waitUntilVisible(element);
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(keys);
+    }
+
+    public void clickMethod(WebElement element){
+        waitUntilClickable(element);
+        scrollToElement(element);
+        element.click();
+    }
+
+    public void verifyEqualsText(WebElement element, String value){
+        waitUntilVisible(element);
+        Assert.assertEquals(element.getText(),value);
+    }
+
+    public void setWait(double second){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void login(){
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7");
+
+        visibilityOfElement(By.xpath("//input[@name='username']"));
+        WebElement username = driver.findElement(By.xpath("//input[@name='username']"));
+        username.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys("admin123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
     }
 
 }
