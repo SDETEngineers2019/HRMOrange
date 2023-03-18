3786c2961c9b8f6ce3c1611e30f70fb579c26160
Belgin <belgin.doan@gmail.com>
2023-03-17 23:06:22 -0400

    Some classes are added and some classes ara updated

--- src/test/java/Utilities/Tools/Tools_Muhammet.java
+++ src/test/java/Utilities/Tools/Tools_Muhammet.java
@@ -1,10 +1,39 @@
 package Utilities.Tools;
 
+import Utilities.ParameterDriverClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
-import Utilities.BaseDriver;
+import java.time.Duration;
 
 
-public class Tools_Muhammet extends BaseDriver {
+public class Tools_Muhammet extends ParameterDriverClass {
+
+    public static WebDriverWait wait;
+
+    public void waitUntilClickable(WebElement element){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void clickMethod(WebElement element){
+        waitUntilClickable(element);
+        element.click();
+    }
+
+    public void waitUntilVisible(WebElement element){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void verifyDisplayedElements(WebElement element){
+        waitUntilVisible(element);
+        Assert.assertTrue(element.isDisplayed(), "Test is failed");
+    }
+
+
 
 }
 
